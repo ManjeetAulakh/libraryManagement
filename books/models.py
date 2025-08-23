3e5601b5e0d149f57350377b8de8bd2ebaa1cb61
Manjeet Singh <118804835+ManjeetAulakh@users.noreply.github.com>
2025-08-23 23:28:47 +0530

    creating Book Model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)       # Book title
+    author = models.CharField(max_length=100)      # Author name
+    published_date = models.DateField(null=True, blank=True)
+    isbn = models.CharField(max_length=13, unique=True)
+    pages = models.IntegerField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.title} by {self.author}"
